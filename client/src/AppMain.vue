337d336c89134ac3be0c2b9bac6aefe98a66049e
Pedro Alves <pedro.alves@webdetails.pt>
2020-01-24 16:29:12 +0000

    - Started playing with this thing…

--- client/src/AppMain.vue
+++ client/src/AppMain.vue
@@ -1,9 +1,9 @@
 <template>
 	<div class="wrapper" id="req_app" >
 		<!-- Sidebar  -->
-		<nav id="sidebar">
+		<nav id="sidebar" v-class="{active:isActive}">
 		    <div class="sidebar-header">
-		        <h3>Workout Companion</h3>
+		        <h3>Workout Companion</h3>XXX
 		    </div>
 		    <ul class="list-unstyled components">
 				<li><router-link to="/"> Home</router-link></li>
@@ -16,16 +16,48 @@
 		        <li><router-link to="/new"> New Request</router-link></li>
 		    </ul>
 		</nav>
+
+
+		<!-- Toggle button -->
+
+
 		<!-- Page Content  -->
 		<div id="content">
+			<div id="sidebarCollapseDiv">
+				
+				<button type="button" id="sidebarCollapse" class="btn btn-info"  @click="toggleSidebar()">
+					<i class="fas fa-align-left"></i>
+					<span>Toggle Sidebar</span>
+				</button>
+			</div>
 			<router-view></router-view>
 		</div>
 	</div>
 </template>
 <script>
+
 import router from './pages/routes';
 export default {
   name: 'AppMain',
-  router
+  router,
+
+  data:{
+	isActive: false
+  },
+  created(){
+	  console.log("Created!")
+  },
+  methods:
+  {
+  	toggleSidebar()
+  	{
+		  console.log("Teest");
+		  this.isActive = !this.isActive;
+
+  	}
+  }
 }
+
+
+
 </script>
